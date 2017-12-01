9f7d6a083edd79601a36f0a09888b0757e49d7d5
Ritchie Lozada <rlozada@microsoft.com>
2017-11-30 17:48:16 -0800

    GestureRecognizer setup to handle Tap and Hold Events

--- Assets/Scripts/HandsTrackingController.cs
+++ Assets/Scripts/HandsTrackingController.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.WSA.Input;
 
@@ -23,16 +24,88 @@ namespace HoloLensHandTracking
         }
 
         public GameObject TrackingObject;
+        public TextMesh StatusText;
+        public Color DefaultColor = Color.green;
+        public Color TapColor = Color.blue;
+        public Color HoldColor = Color.red;
+
         private HashSet<uint> trackedHands = new HashSet<uint>();
         private Dictionary<uint, GameObject> trackingObject = new Dictionary<uint, GameObject>();
+        private GestureRecognizer gestureRecognizer;
+        private uint activeId;
 
         void Awake()
         {
             InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
             InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
             InteractionManager.InteractionSourceLost += InteractionManager_InteractionSourceLost;
+
+            gestureRecognizer = new GestureRecognizer();
+            gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.Hold);
+            gestureRecognizer.Tapped += GestureRecognizerTapped;
+            gestureRecognizer.HoldStarted += GestureRecognizer_HoldStarted;
+            gestureRecognizer.HoldCompleted += GestureRecognizer_HoldCompleted;
+            gestureRecognizer.HoldCanceled += GestureRecognizer_HoldCanceled;            
+            gestureRecognizer.StartCapturingGestures();
+            StatusText.text = "READY";
+        }
+
+        void ChangeObjectColor(GameObject obj, Color color)
+        {            
+            var rend = obj.GetComponentInChildren<Renderer>();
+            if (rend)
+            {
+                rend.material.color = color;
+                Debug.LogFormat("Color Change: {0}", color.ToString());
+            }
+        }
+
+
+        private void GestureRecognizer_HoldStarted(HoldStartedEventArgs args)
+        {
+            uint id = args.source.id;            
+            StatusText.text = $"HoldStarted - Kind:{args.source.kind.ToString()} - Id:{id}";
+            if (trackingObject.ContainsKey(activeId))
+            {
+                ChangeObjectColor(trackingObject[activeId], HoldColor);
+                StatusText.text += "-TRACKED";
+            }
+        }
+
+        private void GestureRecognizer_HoldCompleted(HoldCompletedEventArgs args)
+        {
+            uint id = args.source.id;            
+            StatusText.text = $"HoldCompleted - Kind:{args.source.kind.ToString()} - Id:{id}";
+            if(trackingObject.ContainsKey(activeId))
+            {
+                ChangeObjectColor(trackingObject[activeId], DefaultColor);
+                StatusText.text += "-TRACKED";
+            }
+        }
+
+        private void GestureRecognizer_HoldCanceled(HoldCanceledEventArgs args)
+        {
+            uint id = args.source.id;            
+            StatusText.text = $"HoldCanceled - Kind:{args.source.kind.ToString()} - Id:{id}";
+            if (trackingObject.ContainsKey(activeId))
+            {
+                ChangeObjectColor(trackingObject[activeId], DefaultColor);
+                StatusText.text += "-TRACKED";
+            }
+        }
+
+        private void GestureRecognizerTapped(TappedEventArgs args)
+        {            
+            uint id = args.source.id;
+            StatusText.text = $"Tapped - Kind:{args.source.kind.ToString()} - Id:{id}";
+            if (trackingObject.ContainsKey(activeId))
+            {
+                ChangeObjectColor(trackingObject[activeId], TapColor);
+                StatusText.text += "-TRACKED";
+            }            
         }
         
+
         private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs args)
         {
             uint id = args.state.source.id;
@@ -40,8 +113,9 @@ namespace HoloLensHandTracking
             if (args.state.source.kind != InteractionSourceKind.Hand)
             {
                 return;
-            }
+            }            
             trackedHands.Add(id);
+            activeId = id;
 
             var obj = Instantiate(TrackingObject) as GameObject;
             Vector3 pos;
@@ -62,7 +136,7 @@ namespace HoloLensHandTracking
 
             if (args.state.source.kind == InteractionSourceKind.Hand)
             {
-                if (trackingObject.ContainsKey(args.state.source.id))
+                if (trackingObject.ContainsKey(id))
                 {
                     if (args.state.sourcePose.TryGetPosition(out pos))
                     {
@@ -97,13 +171,23 @@ namespace HoloLensHandTracking
                 trackingObject.Remove(id);
                 Destroy(obj);
             }
+            if (trackedHands.Count > 0)
+            {
+                activeId = trackedHands.First();
+            }
         }
 
         void OnDestroy()
-        {            
+        {                        
             InteractionManager.InteractionSourceDetected -= InteractionManager_InteractionSourceDetected;
             InteractionManager.InteractionSourceUpdated -= InteractionManager_InteractionSourceUpdated;
             InteractionManager.InteractionSourceLost -= InteractionManager_InteractionSourceLost;
+
+            gestureRecognizer.Tapped -= GestureRecognizerTapped;
+            gestureRecognizer.HoldStarted -= GestureRecognizer_HoldStarted;
+            gestureRecognizer.HoldCompleted -= GestureRecognizer_HoldCompleted;
+            gestureRecognizer.HoldCanceled -= GestureRecognizer_HoldCanceled;
+            gestureRecognizer.StopCapturingGestures();
         }
     }
 }
--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 
 - Unity 2017.2.0f3
 - Windows SDK 10.0.16299.0
-- Windows 10 Version 1709 OS Build 16299.19
+- Windows 10 Version 1709 OS Build 16299
 
 ### HoloLens Tested Version
 
 - Windows version	14393.1770.x86fre.rs1_release.170917-1700
 - Windows version	14393.1884.x86fre.rs1_release.171101-1233
+
+### Release Notes 11/30/2017
+- Included GestureRecognizer to handle Tap/Hold Events
+- GestureRecognizer Tap/Hold Events does not create a "Hand" InteractionSourceKind and Source ID is "0", the event cannot map to previous Hand Detection Events
+- The "Tap" event does not have a "Tap Release", the workaround is to use "HoldStarted" and "HoldCompleted" to detect the release equivalent
 
 ### Release Notes 11/14/2017
 - Updated Project for Unity 2017.2.0f3 / Windows 10.0.16299.0 SDK
